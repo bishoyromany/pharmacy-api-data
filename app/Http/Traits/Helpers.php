c615363e39bbe345c6e9165f84bf8646b75ec8ac
Bishoy Romany <7bishoyromany7@gmail.com>
2021-08-01 09:18:57 -0400

    Added Where, Order Options

--- app/Http/Traits/Helpers.php
+++ app/Http/Traits/Helpers.php
@@ -9,15 +9,29 @@ trait Helpers
         $page = (int)$request->page ?? 1;
         $perpage = (int)$request->perpage ?? 20;
         $offset = ($page - 1) * $perpage;
+        $where = $request->where ?? false;
+        $order = $request->order ?? "ASC";
+        $orderBy = $request->orderBy ?? false;
+        $query = $this->table;
+        if($where){
+            $query = $query->where($where, $request->value);
+        }
+
+        if($orderBy){
+            $query = $query->orderBy($orderBy, $order);
+        }
+
+        $counter = $query;
+        $query = $query->limit($perpage)->offset($offset);
 
         return [
             'pagination' => [
                 'page' => $page,
                 'perpage' => $perpage,
                 'nextPage' => $page + 1,
-                'total' => $this->table->count()
+                'total' => $counter->count()
             ],
-            'data' => $this->table->limit($perpage)->offset($offset)->get()
+            'data' => $query->get()
         ];
         dd($request->all());
     }
