67ab016e04f6b6cd909d04b2731a50d85d1af1d5
Bishoy Romany <7bishoyromany7@gmail.com>
2021-11-21 19:33:39 -0500

    Increase API Rate Limit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:api',
+            'throttle:100000,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
