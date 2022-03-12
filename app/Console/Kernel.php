ca2d3f820c31e389fa52b676df5e073d57c92c21
Bishoy Romany <7bishoyromany7@gmail.com>
2022-03-12 14:36:34 -0500

    PreventOver lapping

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends ConsoleKernel
             fwrite($fp, 'Cron Alive ' . date('Y-m-d H:i:s A') . " \n");
             fclose($fp);
         })->everyMinute()->name("Cron Is Active");
-        $schedule->job(new PharmacySync)->everyTenMinutes()->name("Data Sync");
+        $schedule->job(new PharmacySync)->everyTenMinutes()->name("Data Sync")->withoutOverlapping();
         // $schedule->command('inspire')->hourly();
     }
 
